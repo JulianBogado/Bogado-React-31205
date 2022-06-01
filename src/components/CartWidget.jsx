cfd67c800c53207f6b01e15a606236e9e66c6192
JulianBogado <98061009+JulianBogado@users.noreply.github.com>
2022-06-01 12:42:19 -0300

    Update CartWidget.jsx

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -4,12 +4,12 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 
-function CartWidget({items}) {
+function CartWidget() {
   return (
       <>
     <Tooltip title="Carrito">
     <IconButton aria-label="cart" color="inherit">
-      <Badge badgeContent={items} color="error">
+      <Badge badgeContent={5} color="error">
         <ShoppingCartIcon />
       </Badge>
     </IconButton>
