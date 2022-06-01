0bacb7a519eecc0e721c6a5e06b9d69e0b597745
JulianBogado <98061009+JulianBogado@users.noreply.github.com>
2022-06-01 12:41:50 -0300

    Update ItemListContainer.jsx

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import ItemCount from './ItemCount'
 
 function ItemListContainer({greeting}) {
   return (
     <>
     <h1>{greeting}</h1>
-{/*     <div>
-    <ItemCount stock={6} initial={1}  />
-  </div> */}
   </>
   )
 }
